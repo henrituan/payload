86e5a2d11d6acc2bcc6def806f15bfb1cacb9939
Henri Tuan <henri.tuan77@gmail.com>
2022-07-01 13:42:49 +0200

    feat: add file option in localApi Create

--- src/collections/operations/local/create.ts
+++ src/collections/operations/local/create.ts
@@ -1,27 +1,31 @@
-import { UploadedFile } from 'express-fileupload';
-import { Payload } from '../../..';
-import { PayloadRequest } from '../../../express/types';
-import { Document } from '../../../types';
-import getFileByPath from '../../../uploads/getFileByPath';
-import create from '../create';
+import { UploadedFile } from "express-fileupload";
+import { Payload } from "../../..";
+import { PayloadRequest } from "../../../express/types";
+import { Document } from "../../../types";
+import getFileByPath from "../../../uploads/getFileByPath";
+import create from "../create";
 
 export type Options<T> = {
-  collection: string
-  data: Record<string, unknown>
-  depth?: number
-  locale?: string
-  fallbackLocale?: string
-  user?: Document
-  overrideAccess?: boolean
-  disableVerificationEmail?: boolean
-  showHiddenFields?: boolean
-  filePath?: string
-  overwriteExistingFiles?: boolean
-  req?: PayloadRequest
-  draft?: boolean
-}
+  collection: string;
+  data: Record<string, unknown>;
+  depth?: number;
+  locale?: string;
+  fallbackLocale?: string;
+  user?: Document;
+  overrideAccess?: boolean;
+  disableVerificationEmail?: boolean;
+  showHiddenFields?: boolean;
+  filePath?: string;
+  file?: File;
+  overwriteExistingFiles?: boolean;
+  req?: PayloadRequest;
+  draft?: boolean;
+};
 
-export default async function createLocal<T = any>(payload: Payload, options: Options<T>): Promise<T> {
+export default async function createLocal<T = any>(
+  payload: Payload,
+  options: Options<T>
+): Promise<T> {
   const {
     collection: collectionSlug,
     depth,
@@ -33,6 +37,7 @@ export default async function createLocal<T = any>(payload: Payload, options: Op
     disableVerificationEmail,
     showHiddenFields,
     filePath,
+    file,
     overwriteExistingFiles = false,
     req,
     draft,
@@ -50,14 +55,19 @@ export default async function createLocal<T = any>(payload: Payload, options: Op
     overwriteExistingFiles,
     draft,
     req: {
-      ...req || {},
+      ...(req || {}),
       user,
-      payloadAPI: 'local',
-      locale: locale || req?.locale || (payload?.config?.localization ? payload?.config?.localization?.defaultLocale : null),
+      payloadAPI: "local",
+      locale:
+        locale ||
+        req?.locale ||
+        (payload?.config?.localization
+          ? payload?.config?.localization?.defaultLocale
+          : null),
       fallbackLocale: fallbackLocale || req?.fallbackLocale || null,
       payload,
       files: {
-        file: getFileByPath(filePath) as UploadedFile,
+        file: file ?? (getFileByPath(filePath) as UploadedFile),
       },
     } as PayloadRequest,
   });
